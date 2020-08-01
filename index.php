762c7cd9c26e5c507e062a3982c120e22c8c9ab4
Robson Miranda <robson1992ferreira@hotmail.com>
2020-08-01 12:23:26 -0300

    template

---
+++ index.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css" integrity="sha512-17EgCFERpgZKcm0j0fEq1YCJuyAWdz9KUtv1EjVuaOz8pDnh/0nZxmU6BBXwaaxqoi9PQXnRWqlcDB027hgv9A==" crossorigin="anonymous" />
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <link rel="stylesheet" type="text/css" href="slick/slick.css"/>
+    <link rel="stylesheet" type="text/css" href="slick/slick-theme.css"/>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>Template</title>
+</head>
+<body>
+    <header class="container">
+        <div class= "row">
+            <div class="col-lg-12">
+                <nav class="navbar navbar-expand-lg navbar-light ">
+                    <a class="navbar-brand float-left" href="#">Navbar</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse " id="navbarNavDropdown">
+                        <ul class="navbar-nav menu ml-auto">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Features</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Pricing</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Pricing</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Pricing</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Pricing</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Pricing</a>
+                            </li>
+                    
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </header>
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+            <img class="d-block w-100" src="img/01.jpg" alt="First slide">
+            </div>
+            <div class="carousel-item">
+            <img class="d-block w-100" src="img/02.jpg" alt="Second slide">
+            </div>
+            <div class="carousel-item">
+            <img class="d-block w-100" src="img/03.jpg" alt="Third slide">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+    <main class= "container">
+        <div class="row">
+            <div class="col-lg-6">
+                <h2>Lorem</h2>
+            </div>
+            <div class="col-lg-6">
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci reprehenderit quaerat, accusamus cupiditate, beatae earum dolorum neque, ut id illo modi consectetur ipsa tempora. Explicabo quam id hic beatae quisquam.</p>
+            </div>
+        
+            <div class="slider col-lg-12">
+                <div class="col-lg-3"><img src="https://i.imgur.com/3aIutJi.png" alt="js" /></div>
+                <div class="col-lg-3"><img src="https://i.imgur.com/rWtGeQ0.png" alt="java" /></div>
+                <div class="col-lg-3"><img src="https://i.imgur.com/8946bSF.png" alt="python"/></div>
+                <div class="col-lg-3"><img src="https://i.imgur.com/9Hc7hjb.png" alt="kotlin" /></div>
+                <div class="col-lg-3"><img src="https://i.imgur.com/uZDEpjX.png" alt="scala" /></div>
+                <div class="col-lg-3"><img src="https://i.imgur.com/eQoyQjy.png" alt="C#" /></div>
+                <div class="col-lg-3"><img src="https://i.imgur.com/ko3iAbu.png" alt="PHP" /></div>
+                <div class="col-lg-3"><img src="https://i.imgur.com/Afkz5od.png" alt="dotnet" /></div>
+            </div>
+
+            <section class=" col-lg-12">
+                <h2>Lorem</h2>
+                
+                        <div class="card col-lg-4 mb-5 float-left">
+                            <img src="img/amsterdam-1200x600-1.jpg" class="card-img-top" alt="img/amsterdam-1200x600-1" width="200px">
+                            <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            </div>
+                            <div class="card-footer">
+                            <small class="text-muted">Last updated 3 mins ago</small>
+                            </div>
+                        </div>
+                        <div class="card col-lg-4 mb-5 float-left">
+                            <img src="img/amsterdam-1200x600-1.jpg" class="card-img-top" alt="img/amsterdam-1200x600-1">
+                            <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                            </div>
+                            <div class="card-footer">
+                            <small class="text-muted">Last updated 3 mins ago</small>
+                            </div>
+                        </div>
+                        <div class="card col-lg-4 mb-5 float-left">
+                            <img src="img/amsterdam-1200x600-1.jpg" class="card-img-top" alt="img/amsterdam-1200x600-1">
+                            <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                            </div>
+                            <div class="card-footer">
+                            <small class="text-muted">Last updated 3 mins ago</small>
+                            </div>
+                        </div>
+                        <div class="card col-lg-4 mb-5 float-left">
+                            <img src="img/amsterdam-1200x600-1.jpg" class="card-img-top" alt="img/amsterdam-1200x600-1">
+                            <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            </div>
+                            <div class="card-footer">
+                            <small class="text-muted">Last updated 3 mins ago</small>
+                            </div>
+                        </div>
+                        <div class="card col-lg-4 mb-5 float-left">
+                            <img src="img/amsterdam-1200x600-1.jpg" class="card-img-top" alt="img/amsterdam-1200x600-1">
+                            <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                            </div>
+                            <div class="card-footer">
+                            <small class="text-muted">Last updated 3 mins ago</small>
+                            </div>
+                        </div>
+                        <div class="card col-lg-4 mb-5 float-left">
+                            <img src="img/amsterdam-1200x600-1.jpg" class="card-img-top" alt="img/amsterdam-1200x600-1">
+                            <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                            </div>
+                            <div class="card-footer">
+                            <small class="text-muted">Last updated 3 mins ago</small>
+                            </div>
+                        </div>
+            </section>
+
+            <section class=" col-lg-12">
+            <form method="POST">
+               <p>Para entrar em contato conosco,preencha o formulario abaixo:</p>
+               <label>
+                   <strong>Nome:</strong>
+                   <input type="text" name="nome" placeholder="Preencha seu nome " required />
+               </label>
+               <label>
+                    <strong>E-mail:</strong>
+                    <input type="email" name="email" placeholder="Preencha seu email " required />
+                </label>
+                <label>
+                    <strong>Assunto:</strong>
+                    <input type="text" name="Assunto" placeholder="Preencha seu Assunto " required />
+                </label>
+                <label>
+                    <strong>mensagem:</strong>
+                    <textarea  name="mensagem" placeholder="Preencha seu mensagem " rows="6"></textarea>
+                </label>
+                <button type="submit" class="btn">
+                        <i class="far fa-paper-plane"></i>
+                    Enviar 
+                </button>
+           </form>
+            </section>
+
+
+            
+        </div>
+    </main>
+
+</body>
+<!-- JS, Popper.js, and jQuery -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+<script src="slick/slick.min.js"></script>
+<script src="js/slider.js"></script>
+</html>
